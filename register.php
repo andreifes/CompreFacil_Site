9848458a3581acf2ff01be28d4a3fd7eb189ecae
André Neves Alves <20171TIIMI0360@cefetes.br>
2022-01-20 19:54:11 -0300

    login e registro

---
+++ register.php
@@ -0,0 +1,48 @@
+<?php
+ 
+/*
+ * Following code will create a new product row
+ * All product details are read from HTTP Post Request
+ */
+ 
+// connecting to db
+$con = pg_connect(getenv("DATABASE_URL"));
+ 
+// array for JSON response
+$response = array();
+ 
+// check for required fields
+if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['telefone']) && isset($_POST['senha'])) {
+ 
+	$newNome = trim($_POST['nome']);
+	$newEmail = trim($_POST['email']);
+	$newTelefone = trim($_POST['telefone']);
+	$newSenha = trim($_POST['senha']);
+		
+	$usuario_existe = pg_query($con, "SELECT email FROM usuarios WHERE email='$newEmail'");
+	// check for empty result
+	if (pg_num_rows($usuario_existe) > 0) {
+		$response["success"] = 0;
+		$response["error"] = "usuario ja cadastrado";
+	}
+	else {
+		// mysql inserting a new row
+		$result = pg_query($con, "INSERT INTO usuarios(nome, email, telefone, senha) VALUES('$newNome', '$newEmail', '$newTelefone', '$newSenha')");
+	 
+		if ($result) {
+			$response["success"] = 1;
+		}
+		else {
+			$response["success"] = 0;
+			$response["error"] = "Error BD: ".pg_last_error($con);
+		}
+	}
+}
+else {
+    $response["success"] = 0;
+	$response["error"] = "faltam parametros";
+}
+
+pg_close($con);
+echo json_encode($response);
+?>

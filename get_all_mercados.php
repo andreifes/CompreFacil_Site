6e6cfb9781d888c70bbf1b1848ef18a32433d3c5
André Neves Alves <20171TIIMI0360@cefetes.br>
2022-01-24 17:03:45 -0300

    mudanca get all mercados

--- get_all_mercados.php
+++ get_all_mercados.php
@@ -8,16 +8,20 @@ $result = pg_query($con, "SELECT *FROM mercado");
 
 if (pg_num_rows($result) > 0) {
 
-    $response["mercado"] = array();
+    $response["mercados"] = array();
 
     while ($row = pg_fetch_array($result)) {
         $mercado = array();
         $mercado["id"] = $row["id"];
         $mercado["nome"] = $row["nome"];
         $mercado["img"] = $row["img"];
-        $mercado["id_endereco"] = $row["id_endereco"];
+		$id_endereco = $row["id_endereco"]
+		$result1 = pg_query($con, "SELECT * FROM endereco WHERE endereco.id = '$id_endereco'");
+		$row1 = pg_fetch_array($result1)
+        $mercado["cidade"] = $row1["cidade"];
+		$mercado["bairro"] = $row1["bairro"];
 
-        array_push($response["mercado"], $mercado);
+        array_push($response["mercados"], $mercado);
     }
 
     $response["success"] = 1;

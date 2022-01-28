b26ec90fe943cd2fc38cc847f2873bbc26e7061f
Joao Matheus <universitariodoano@gmail.com>
2022-01-28 10:10:45 -0300

    recorrigido erro parenteses 2x

--- get_compras.php
+++ get_compras.php
@@ -8,7 +8,6 @@ if (isset($_GET["email_usuario"])) {
 	// Aqui sao obtidos os parametros
     $email = $_GET['email_usuario'];
  
-    echo $email;
 	$result = pg_query($con, "SELECT * FROM compra WHERE email_usuario = '$email'");
 	$response["compras"] = array();
 	
